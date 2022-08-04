40a9e4cd64ad8ca27da045fab97ab124cba81b6b
akramshaikh2000 <91551068+akramshaikh2000@users.noreply.github.com>
2022-08-04 13:02:26 +0530

    Add files via upload

---
+++ lab practical/VowCons.java
@@ -0,0 +1,28 @@
+class VowCons {
+
+  public static void main(String[] args) {
+    String line = "My Name is Mohammed Akram N Shaikh";
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+    line = line.toLowerCase();
+    for (int i = 0; i < line.length(); ++i) {
+      char ch = line.charAt(i);
+
+      
+      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        ++vowels;
+      }
+
+      
+      else if ((ch >= 'a' && ch <= 'z')) {
+        ++consonants;
+      }
+      
+     
+    }
+
+    System.out.println("Vowels: " + vowels);
+    System.out.println("Consonants: " + consonants);
+    
+  }
+}
